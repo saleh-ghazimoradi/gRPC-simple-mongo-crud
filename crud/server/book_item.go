07727fe25ba28f60ad8ed776ef339ef3ca47fea2
Saleh Ghazimoradi <74179371+saleh-ghazimoradi@users.noreply.github.com>
2023-10-24 13:45:52 +0330

    add omitempty to the bson for the ID

--- crud/server/book_item.go
+++ crud/server/book_item.go
@@ -6,7 +6,7 @@ import (
 )
 
 type BookItem struct {
-	ID       primitive.ObjectID `bson:"_id"`
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
 	AuthorId string             `bson:"author_id"`
 	Title    string             `bson:"title"`
 	Content  string             `bson:"content"`
